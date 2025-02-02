371152dd79804332ec12724d774b9fad78dc4db0
Fiza Awan <162214280+iziawan@users.noreply.github.com>
2025-02-02 18:17:59 +0500

    Update question-2.ts

--- question2/question-2.ts
+++ question2/question-2.ts
@@ -2,5 +2,5 @@
 // and print a message to that person. Your message should
  //be simple, such as,“Hello Eric, would you like to learn some Python today?”
  
-let person = 'Shahabuddin';
+let person = 'Fiza Awan';
 console.log(`Hello ${person} would you like to learn some Python?`);
